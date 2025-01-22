c6f56bc4630728111ffdfa01c71f7a1e8023e810
Neeraj Singh Parihar <59050373+neeraj-sp22@users.noreply.github.com>
2025-01-22 17:01:48 +0530

    Update JenkinsDemoApplication.java

--- src/main/java/com/jenkins/test/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/test/JenkinsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Project JenkinsDemoApplication started.");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
